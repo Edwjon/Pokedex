cfbdbc1b1d6f098edc5c8f660d44c94fd0c42c64
Edward Pizzurro <edwardpizzurro@gmail.com>
2025-03-23 12:20:06 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Edward Pizzurro on 3/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.green
+    }
+
+
+}
+
