1b0fba385208fb782bec524e6a66c0a6abb4914d
Edward Pizzurro <edwardpizzurro@gmail.com>
2025-03-24 22:57:25 -0400

    Scene Delegate main logic to start the app

--- Application/SceneDelegate.swift
+++ Application/SceneDelegate.swift
@@ -13,10 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+        let window = UIWindow(windowScene: windowScene)
+        let rootVC = PokemonListViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        window.rootViewController = navVC
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
